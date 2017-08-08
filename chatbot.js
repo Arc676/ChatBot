88f43ea96b30723460a9d72f3acb1a0a2171694b
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-08-08 22:33:46 +0800

    Regex match and single capture group supported, plus more jokes

--- chatbot.js
+++ chatbot.js
@@ -5,6 +5,16 @@ var callAndResponse = [
 	["I love you", "I know"],
 	["ping", "pong"],
 	["This is madness!", "Madness?  **THIS IS SPARTA!**"],
+	["There isn't enough road", "Where we're going, we don't need roads."],
+	["May the force be with you", "And also with you"],
+	["Is this Tony Stank\\?", "Yes, this is the Tony Stank"],
+	["[wW]here [ia][sr]e? .+\\?", "Somewhere, over the rainbow"],
+	["[wW]hat is .+", "42"],
+	["Why did the chicken cross the road\?", "To get to the other side"],
+	["LICENSE", "MIT License at https://github.com/Arc676/ChatBot"],
+	["Watcha doin\\?", "Eatin' chocolate"],
+	["Whered'ya get it\\?", "A dog dropped it!"],
+	["This isn't your average, everyday (.+)", "This is **ADVANCED $**"]
 ];
 
 var shouldLeave = false;
@@ -20,20 +30,29 @@ client.on('ready', () => {
 
 function getReply(msg){
 	
-	//Loop through array and get the appropriate response to the call
-	for (var i = 0; i < callAndResponse.length; i++){
-		if (msg == callAndResponse[i][0]){
-			return callAndResponse[i][1];
-		}	
-	}
-	
+	//Check for basic commands first (switch is supposedly faster, so leave the for-loop for later)
 	//Checks for commands (Add chatbot + commands support later)
 	switch (msg) {
 	case "chatbothelp":
-		return "Recognized commands:\n\tping\n\tchatbot\n\tchatbothelp\n\tchatbotgoaway\n\tchatbotstay"
+		return "Recognized commands:\n\tping\n\tchatbot\n\tchatbothelp\n\tchatbotgoaway\n\tchatbotstay\n\t(various movie quotes)"
 	default:
-		return "";
+		break;
 	}
+	
+	//Loop through array and get the appropriate response to the call
+	for (var i = 0; i < callAndResponse.length; i++){
+		var regexp = new RegExp(callAndResponse[i][0]);
+		if (regexp.test(msg)){
+			response = callAndResponse[i][1];
+			if (callAndResponse[i][0].indexOf("(") !== -1) {
+				var match = regexp.exec(msg);
+				response = response.replace("$", match[1]);
+			}
+			return response;
+		}
+	}
+	
+	return "";
 }
 
 client.on('message', message => {
