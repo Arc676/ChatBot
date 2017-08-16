b4c23a187a54be7aef12153e8d44c12eca345e07
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-08-16 21:34:49 +0800

    Removed unnecessary semicolons

--- io.js
+++ io.js
@@ -20,8 +20,8 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 var callAndResponse = [
 	["I love you", "I know"],
@@ -39,65 +39,65 @@ var callAndResponse = [
 	["This isn't your average, everyday (.+)", "This is **ADVANCED** $"],
 	["How many (\\w+)", "ALL THE $"],
 	["thank.+\\Wio\\W|thank.+\\Wio$", "You are very welcome"]
-];
+]
 
 var name = "io"
-var active = true;
+var active = true
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.username}`);
-//	client.sendMessage("ChatBot has arrived!");
-});
+	console.log(`Logged in as ${client.user.username}`)
+//	client.sendMessage("ChatBot has arrived!")
+})
 
 function getReply(msg){
 	
 	//Loop through array and get the appropriate response to the call
 	for (var i = 0; i < callAndResponse.length; i++){
-		var regexp = new RegExp(callAndResponse[i][0], 'i');
+		var regexp = new RegExp(callAndResponse[i][0], 'i')
 		if (regexp.test(msg)){
-			response = callAndResponse[i][1];
+			response = callAndResponse[i][1]
 			if (callAndResponse[i][0].indexOf("(") !== -1) {
-				var match = regexp.exec(msg);
-				response = response.replace("$", match[1]);
+				var match = regexp.exec(msg)
+				response = response.replace("$", match[1])
 			}
-			return response;
+			return response
 		}
 	}
 	
-	return "";
+	return ""
 }
 
 client.on('message', message => {
-	if (message.author.bot) return;
+	if (message.author.bot) return
 	if (!active) {
 		if (message.content === 'Io come back') {
-			message.reply("I'm back");
-			active = true;
+			message.reply("I'm back")
+			active = true
 		}
-		return;
+		return
 	}
 	if (message.content === name) {
-		message.reply("Hi there!");
+		message.reply("Hi there!")
 	} else if (message.content.startsWith(name + ", ")) {
 		if (message.content.endsWith(' go away')) { //Makes chatbot leave
-			message.reply('OK :(');
+			message.reply('OK :(')
 			active = false
 		} else if (message.content.endsWith(' help')) {
-			message.reply("Recognized commands:\n\thelp\n\tgo away\n\tcome back\n\tdie\n\trtfm\n\t(various movie quotes)");
+			message.reply("Recognized commands:\n\thelp\n\tgo away\n\tcome back\n\tdie\n\trtfm\n\t(various movie quotes)")
 		} else if (message.content.endsWith(' die')) {
-			client.destroy();
-			process.exit(0);
+			client.destroy()
+			process.exit(0)
 		} else if (message.content.endsWith(' rtfm')) {
-			message.reply("Follow your own advice: https://discord.js.org/#/docs/main/stable/general/welcome");
+			message.reply("Follow your own advice: https://discord.js.org/#/docs/main/stable/general/welcome")
 		} else {
-			message.reply("Yes?");
+			message.reply("Yes?")
 		}
 	} else { //Runs commands and performs call and response
-		var response = getReply(message.content);
+		var response = getReply(message.content)
 		if (response !== "") {
-			message.reply(response);
+			message.reply(response)
 		}
 	}
-});
+})
 
-client.login('MzQ0NDMwNzk0NzYyMzU0Njg5.DGsnyA.J9vVgXAqO7YRz4rZGOyAmu9dTFE');
+client.login('MzQ0NDMwNzk0NzYyMzU0Njg5.DGsnyA.J9vVgXAqO7YRz4rZGOyAmu9dTFE')
