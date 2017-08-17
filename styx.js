7a8261e55012b4a247adcb08c8cb3e9a444a22ca
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-08-17 17:25:31 +0800

    Added Styx

---
+++ styx.js
@@ -0,0 +1,71 @@
+//MIT License
+
+//Copyright (c) 2017 Arc676/Alessandro Vinciguerra
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+const fs = require('fs')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+var name = "styx"
+
+var opponent = ""
+var delta = 0
+var guess = 0
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.username}`)
+})
+
+client.on('message', message => {
+	if (message.mentions.users.exists('username', name)) {
+		if (message.content.endsWith("die")) {
+			client.destroy()
+			process.exit(0)
+		} else if (message.content.indexOf("play") !== -1) {
+			var args = message.content.split(' ')
+			if (args.length < 5) {
+				message.reply("Usage: styx play username lowBound upBound [verbose]")
+				return
+			}
+			opponent = args[2]
+			var low = parseInt(args[3])
+			var high = parseInt(args[4])
+			if (args.length > 5 && args[5] === "verbose") {
+				message.channel.send(opponent + " newgame " + low + " " + high)
+			}
+			guess = (high - low) / 2 + low
+			delta = guess / 2
+		} else if (message.content.endsWith("low")) {
+			guess += delta
+			if (delta > 1) {
+				delta /= 2
+			}
+		} else if (message.content.endsWith("high")) {
+			guess -= delta
+			if (delta > 1) {
+				delta /= 2
+			}
+		}
+		else { message.reply("no command, but I recognized the mention!") }
+	}
+})
+
+client.login(fs.readFileSync(name + '.token', 'utf8'))
